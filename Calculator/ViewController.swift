589922740261df2f455063616ecd48db80814426
matthiasvanwoensel <matthias@fluvio.be>
2022-09-07 10:38:23 +0200

    setup calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,11 +12,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    var isFinishedTypingNumber: Bool = true
+    
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
+        
+        isFinishedTypingNumber = true
     
     }
 
@@ -24,6 +28,18 @@ class ViewController: UIViewController {
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
+        
+        if let numValue = sender.currentTitle{
+            
+            if isFinishedTypingNumber{
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            }else{
+                displayLabel.text?.append(numValue)
+            }
+            
+            
+        }
     
     }
 
