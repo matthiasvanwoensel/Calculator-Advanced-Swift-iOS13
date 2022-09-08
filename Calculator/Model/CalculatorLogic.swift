2532585978b61f3287e40fb3d74e0fbcba49c99e
matthiasvanwoensel <matthias@fluvio.be>
2022-09-08 10:31:00 +0200

    afwerking

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -12,28 +12,53 @@ struct CalculatorLogic {
     
     private var number: Double?
     
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
     mutating func setNumber(_ n: Double){
          self.number = n
     }
     
-    func calculate(symbol: String) -> Double? {
+    mutating func calculate(symbol: String) -> Double? {
     
         if let n = number {
-            if symbol == "+/-"{
-               return n * -1
-            } else if symbol == "AC"{
+            switch symbol{
+            case "+/-":
+                return n * -1
+            case "AC":
                 return 0
-            } else if symbol == "%" {
+            case "%":
                 return n * 0.01
+            case "=":
+                return performTwoNumberCalculation(n2: n)
+            default:
+                intermediateCalculation = (n1: n, calcMethod: symbol)
             }
-            else if symbol == "+" {
-               
-            }
-            else if symbol == "=" {
-               
-            }
+            
+            
         }
            
+        return nil
+        
+    }
+    
+    private func performTwoNumberCalculation(n2: Double) -> Double? {
+        
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+            
+            switch operation{
+            case "+" :
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("The operation passed in does not match any of the cases.")
+            }
+        }
+        
         return nil
         
     }
